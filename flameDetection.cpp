1bf23e76958952041a21913fe37d1f19ecd20bbc
theocs2 <theocs2@illinois.edu>
2025-10-28 17:53:08 -0500

    Add project description for flame detection
    Added project description and context for flame detection system.

--- flameDetection.cpp
+++ flameDetection.cpp
@@ -1,3 +1,7 @@
+/*
+Below is a personal project I worked on during the 2024-2025 school year. I utilized an Arduino One, ESP 32 Module, KY-026 Flame Sensor, and made use of several 
+C++ libraries to give status updates to the user on their winter fire's status.
+*/
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
